49b1dde78e61c085c3c77b0135f933d7f2e04b17
garance.wetzel <garance.wetzel@colorz.fr>
2024-03-12 09:55:42 +0100

    :art: [mosaic card]

--- src/components/molecules/Card.jsx
+++ src/components/molecules/Card.jsx
@@ -3,66 +3,40 @@ import { Figure } from '../atoms'
 
 export default function Card ({item}) {
   const slideRef = useRef(null);
-  const THRESHOLD = 5;
 
   const [clientWidth, setClientWidth] = useState(350)
-  const [rotateY, setRotateY] = useState(0)
-  const [rotateX, setRotateX] = useState(0)
 
-
-  const handleHover = (e, card) => {
-    const { clientX, clientY, currentTarget } = e;
-    const { clientWidth, clientHeight, offsetLeft, offsetTop } = currentTarget;
-    setClientWidth(clientWidth)
-    var rect = currentTarget.getBoundingClientRect();
-    const horizontal = (clientX - rect.left) / clientWidth;
-    const vertical = (clientY - offsetTop) / clientHeight;
-
-    const rotateX = (THRESHOLD / 2 - horizontal * THRESHOLD).toFixed(2);
-    const rotateY = (vertical * THRESHOLD - THRESHOLD / 2).toFixed(2);
-
-    setRotateX(rotateX)
-    setRotateY(rotateY)
-  }
-   
-
-  const resetStyles = (e) => {
-    setRotateX(0)
-    setRotateY(0)
-    setClientWidth(e.currentTarget.clientWidth)
-  }
 
   return (
     <div 
       className="card"
       ref={slideRef}
-      onMouseMove={(e) => handleHover(e, slideRef)}
-      onMouseLeave={(e) => resetStyles(e, slideRef)}
-      style={{ transform: `perspective(${clientWidth}px) rotateX(${rotateY}deg) rotateY(${rotateX}deg) scale3d(1, 1, 1)`}}>
+    >
         <div 
-          className=" content bg-dark w-[300px] h-[375px] md:w-[350px] md:h-[425px] text-white border group border-white-opacity-1 duration-200 rounded overflow-hidden p-2 ml-6 xl:ml-10 flex flex-col">
-          <div className="preview h-1/2 md:h-[60%] rounded overflow-hidden">
+          className="content w-full aspect-[5/6] text-white border group border-white-opacity-1 duration-200 rounded overflow-hidden p-2 flex flex-col glassmorphism dark">
+          <div className="preview h-1/2 md:h-[50%] rounded overflow-hidden">
             <Figure 
-              classNames="w-full h-full object-cover object-center"
+              classNames="w-full h-full object-cover object-center opacity-70 group-hover:opacity-100"
               webp={item.srcWebp}
               jpg={item.srcJpg}
             ></Figure>
           </div>
-          <div className="p-2 flex flex-col justify-between flex-1">
+          <div className="px-2 py-4 flex flex-col justify-between flex-1">
             <div>
-              <h3 className="text-lg font-semibold">{item.name}</h3>
-              <div className="space-y-2">
+              <h4 className="text-xl font-semibold mb-2">{item.name}</h4>
+              <ul className="space-y-2">
                 {item.skills.map((skill, i) => (
-                  <div key={i} className={`label mr-2 bg-white-opacity-1 `}>{skill}</div>
+                  <li key={i} className={`label mr-2 bg-white-opacity-1 `}>{skill}</li>
                 ))}
-              </div>
+              </ul>
             </div>
-              {item.link && (
-                <a href={item.link} target='_blank' className="text-sm text-right">
-                  Voir le site
+            {item.link && (
+              <div >
+                <a href={item.link} target='_blank' className="link text-md">
+                  Naviguer vers le site
                 </a>
-  
-              )}
+              </div>
+            )}
           </div>
         </div>
     
---
+++ src/components/organisms/CardMosaic.jsx
@@ -0,0 +1,124 @@
+import React, { useRef, useEffect } from "react";
+import { gsap } from "gsap";
+import { Draggable } from "gsap/Draggable";
+gsap.registerPlugin(Draggable);
+
+
+import Card from '../molecules/Card'
+
+import colorzWebp from '/images/projets/colorz.webp'
+import colorzJpg from '/images/projets/colorz.jpg'
+
+import mathonWebp from '/images/projets/mathon.webp'
+
+import jottWebp from '/images/projets/jott.webp'
+import jottJpg from '/images/projets/jott.jpg'
+
+import lecycloWebp from '/images/projets/lecyclo.webp'
+
+import empreintesWebp from '/images/projets/empreintes.webp'
+import empreintesJpg from '/images/projets/empreintes.jpg'
+
+import lavantGardisteWebp from '/images/projets/lavant-gardiste.webp'
+import lavantGardisteJpg from '/images/projets/lavant-gardiste.jpg'
+
+import uptooWebp from '/images/projets/uptoo.webp'
+
+import vejaJpg from '/images/projets/veja.jpg'
+
+import uptooBlogJpg from '/images/projets/uptooBlog.jpg'
+
+import jcLanieceJpg from '/images/projets/jcLaniece.jpg'
+
+import mauriceJpg from '/images/projets/maurice.jpg'
+
+const slides = [
+  {
+    name: 'Colorz',
+    link: 'https://www.colorz.fr/',
+    srcWebp: colorzWebp,
+    srcAvif: null,
+    srcJpg: colorzJpg,
+    skills: ['Vue.js', 'Nuxt.js', 'Tailwind']
+  },
+  {
+    name: 'Mathon',
+    link: 'https://www.mathon.fr/',
+    srcWebp: mathonWebp,
+    skills: ['Shopify', 'Tailwind', 'Javascript', 'Storyblok', 'Liquid'],
+  },
+  {
+    name: 'Veja BtoB',
+    link: false,
+    disabled: true,
+    srcJpg: vejaJpg,
+    skills: ['Vue.JS', 'Nuxt', 'Djust', 'SCSS']
+  },
+  {
+    name: 'Jott',
+    link: 'https://jott.com/',
+    srcWebp: jottWebp,
+    srcJpg: jottJpg,
+    skills: ['Shopify', 'SCSS', 'Javascript', 'Storyblok', 'Liquid'],
+  },
+
+  {
+    name: 'Empreintes',
+    link: 'https://www.empreintes-paris.com/',
+    srcWebp: empreintesWebp,
+    srcJpg: empreintesJpg,
+    skills: ['Shopify', 'Tailwind', 'Javascript', 'Storyblok', 'Liquid'],
+  },
+  {
+    name: "L'avant gardiste",
+    link: 'https://www.lavantgardiste.com/',
+    srcWebp: lavantGardisteWebp,
+    srcJpg: lavantGardisteJpg,
+    skills: ['Shopify', 'SCSS', 'Javascript', 'Storyblok', 'Liquid'],
+  },
+  {
+    name: 'Le cyclo',
+    link: 'https://www.lecyclo.com/',
+    srcWebp: lecycloWebp,
+    skills: ['Shopify', 'Tailwind', 'Javascript', 'Storyblok', 'Liquid'],
+  },
+  {
+    name: 'Uptoo Blog',
+    link: 'https://uptoo.fr/blog/',
+    srcJpg: uptooBlogJpg,
+    skills: ['React', 'Next.js', 'Tailwind', 'Headless UI', 'Strapi'],
+  },
+  {
+    name: 'Uptoo',
+    link: 'https://uptoo.fr/',
+    srcWebp: uptooWebp,
+    skills: ['React', 'Next.js', 'Tailwind', 'Headless UI', 'Strapi'],
+  },
+  {
+    name: 'Maurice et les autres',
+    link: 'https://mauriceetlesautres.com/',
+    srcJpg: mauriceJpg,
+    skills: ['Conception', 'UI/UX']
+  },
+  {
+    name: 'Jean-Christophe Lanièce',
+    link: 'https://jeanchristophelaniece.com/',
+    srcJpg: jcLanieceJpg,
+    skills: ['Cargo', 'UI/UX']
+
+  }
+]
+
+export default function Slider () {
+
+ 
+  return (
+      <ul className="grid grid-cols-1 md:grid-cols-2 xl:grid-cols-3 2xl:grid-cols-4 gap-6 mx-20">
+        {slides.map((item, index) => (
+          <li key={index}>
+            <Card item={item}></Card>
+          </li>
+        ))}
+      </ul>
+  );
+};
